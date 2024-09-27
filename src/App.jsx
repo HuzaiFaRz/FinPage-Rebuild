997f0e191111ff339dc94b9f8d4a189a86ae1d70
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-09-27 20:46:46 +0500

    Some

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import Paragraph from "./Paragraph";
 function App() {
   return (
     <Fragment>
-      <main className="main bg-Main_Bg bg-cover bg-center bg-no-repeat bg-scroll w-full h-screen saturate-100 font-dmsans">
+      <main className="main z-10 bg-Main_Bg bg-cover bg-center bg-no-repeat bg-scroll w-full h-screen font-dmsans">
         <div className="main_overlay w-full h-screen bg-[#0000004d] flex flex-col items-center justify-between">
           <Navbar />
           <div className="main_page w-full flex flex-col items-start justify-center gap-5 h-max  pb-60 px-16">
