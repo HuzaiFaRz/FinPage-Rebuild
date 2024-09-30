b36a9f788adf4e4582abaa004c6c35ac1a665ed9
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-09-30 15:27:24 +0500

    S

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -13,12 +13,15 @@ function Navbar() {
     setNavbarOpen(!navbarOpen);
   };
 
+  const navbarDefaultClass =
+    "OffCanvas_Links_Ul flex flex-col justify-start items-start gap-5 fixed border-none bg-[#000] lg:Navbar_Links_Ul lg:flex lg:flex-row lg:justify-center lg:w-auto lg:h-auto lg:py-2 lg:px-12 lg:bg-[#ffffff33] lg:backdrop-blur-xl lg:rounded-3xl border lg:border-[#ffffff33] lg:start-1/2 -translate-x-2/4 lg:top-8";
+
   return (
     <Fragment>
-      <nav className="Navbar w-full h-24 lg:h-28 bg-zinc-600 flex row justify-between items-center px-6 xl:px-14 font-dmsans cursor-pointer fixed lg:relative">
+      <nav className="Navbar w-full h-24 z-30 lg:h-28 bg-[#000] lg:bg-transparent flex row justify-between items-center px-6 xl:px-14 font-dmsans cursor-pointer fixed lg:relative">
         {/* Navbar Start */}
 
-        <div className="Navbar_Start_Content z-40 left-8 top-8">
+        <div className="Navbar_Start_Content">
           <a href="/">
             <img
               src={Navbar_Logo}
@@ -29,19 +32,22 @@ function Navbar() {
 
         {/* Navbar Center */}
 
-        <div className="Navbar_Center_Content">
+        <div className="Navbar_Center_Content h-full">
           <ul
-            style={{ transition: "all 0.4s linear" }}
-            className={
+            style={{ transition: "all 0.3s ease-in-out" }}
+            className={`${navbarDefaultClass} ${
               navbarOpen
-                ? "OffCanvas_Links_Ul flex flex-col justify-start items-start gap-5 fixed w-full h-screen left-1/2 top-0 z-30 px-20 py-40 bg-[#000] border-none lg:Navbar_Links_Ul lg:flex lg:flex-row lg:justify-center lg:w-auto lg:h-auto lg:py-2 lg:px-12 lg:bg-[#ffffff33] lg:backdrop-blur-xl lg:rounded-3xl border  lg:border-[#ffffff33] lg:start-1/2 -translate-x-2/4 lg:top-8"
-                : "OffCanvas_Links_Ul flex flex-col justify-start items-start gap-5 fixed w-full h-screen left-1/2 -top-full z-30 px-20 py-40 bg-[#000] border-none lg:Navbar_Links_Ul lg:flex lg:flex-row lg:justify-center lg:w-auto lg:h-auto lg:py-2 lg:px-12 lg:bg-[#ffffff33] lg:backdrop-blur-xl lg:rounded-3xl border  lg:border-[#ffffff33] lg:start-1/2 -translate-x-2/4 lg:top-8"
-            }
+                ? "w-full h-full left-1/2 top-24"
+                : "w-full h-0 left-1/2 top-24"
+            }`}
           >
             {linksArray.map((element, index) => (
               <li
                 key={index}
-                className="Navbar_Li text-2xl tracking-tight text-[#ffffffcc] hover:text-[#ffffff] px-2 py-2  lg:text-sm "
+                style={{ transition: "all 0.3s ease-in-out" }}
+                className={`Navbar_Li text-2xl tracking-tight py-8 px-10 mt-5 text-[#ffffffcc] hover:text-[#ffffff] lg:mt-0 lg:px-2 lg:py-2 lg:text-sm ${
+                  navbarOpen ? "opacity-1" : "opacity-0 -mb-5"
+                }`}
               >
                 <a href="#" className="Navbar_Link px-2 py-2">
                   {element}
@@ -53,7 +59,7 @@ function Navbar() {
 
         {/* Navbar End */}
 
-        <div className="Navbar_End_Content z-40">
+        <div className="Navbar_End_Content">
           <a
             href="#"
             className="hidden flex-row justify-center items-center gap-2 px-2 py-2 relative lg:flex"
@@ -63,13 +69,13 @@ function Navbar() {
           </a>
 
           <div
-            className="flex flex-col justify-center items-end gap-1 relative lg:hidden"
+            className="flex flex-col justify-center items-end gap-1 relative px-5 py-5 lg:hidden"
             onClick={NavbrOpenFunctionility}
           >
             {navbarOpen ? (
               <>
-                <span className="w-14 h-1 border-t-2 -rotate-45"></span>
-                <span className="w-14 h-1 border-t-2 rotate-45"></span>
+                <span className="w-6 md:w-12 h-1 border-t-2 -rotate-45"></span>
+                <span className="w-6 md:w-12 h-1 border-t-2 rotate-45"></span>
               </>
             ) : (
               <>
@@ -77,6 +83,24 @@ function Navbar() {
                 <span className="w-7 md:w-9 h-1 border-t-2"></span>
               </>
             )}
+
+            {/* <button x-data="{ open: false }"
+          x-on:click="open = !open"
+          class="flex items-center space-x-2 focus:outline-none">
+    <span x-text="open ? 'Close' : 'Open'"
+          class="font-medium text-lg">Open</span>
+    
+    <div class="w-6 flex items-center justify-center relative">
+      <span x-bind:class="open ? 'translate-y-0 rotate-45' : '-translate-y-2'"
+            class="transform transition w-full h-px bg-current absolute"></span>
+      
+      <span x-bind:class="open ? 'opacity-0 translate-x-3' : 'opacity-100'"
+            class="transform transition w-full h-px bg-current absolute"></span>
+      
+      <span x-bind:class="open ? 'translate-y-0 -rotate-45' : 'translate-y-2'"
+            class="transform transition w-full h-px bg-current absolute"></span>
+    </div>
+  </button> */}
 
             {/* <input
               type="checkbox"
