1e96a2798058543c01410731c46fc36e2289fa67
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-10-01 12:29:56 +0500

    Some

--- src/Heading.jsx
+++ src/Heading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Heading({ text }) {
   return (
-    <h1 className="text-8xl text-white font-sora font-light tracking-tighter w-2/4">
+    <h1 className="text-5xl w-full sm:text-6xl md:text-7xl lg:text-8xl xl:w-2/4 text-white font-sora font-light tracking-tighter">
       {text}
     </h1>
   );
--- src/Paragraph.jsx
+++ src/Paragraph.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Paragraph({ text }) {
   return (
-    <p className="text-lg  text-[#ffffffb3] font-sora font-light tracking-wide leading-6 w-64">
+    <p className="w-full xl:w-[50%] text-lg  text-[#ffffffb3] font-sora font-light tracking-wide leading-6 ">
       {text}
     </p>
   );
