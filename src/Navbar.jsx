93a477bc141eb579ae612ed06cad43c1f743801f
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-10-01 12:01:50 +0500

    Some

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -9,12 +9,11 @@ function Navbar() {
   const [navbarOpen, setNavbarOpen] = useState(false);
 
   const NavbrOpenFunctionility = () => {
-    console.log(this);
     setNavbarOpen(!navbarOpen);
   };
 
-  const navbarDefaultClass =
-    "OffCanvas_Links_Ul flex flex-col justify-start items-start gap-5 fixed border-none bg-[#000] lg:Navbar_Links_Ul lg:flex lg:flex-row lg:justify-center lg:w-auto lg:h-auto lg:py-2 lg:px-12 lg:bg-[#ffffff33] lg:backdrop-blur-xl lg:rounded-3xl border lg:border-[#ffffff33] lg:start-1/2 -translate-x-2/4 lg:top-8";
+  const navbarDefaultClass = `OffCanvas_Links_Ul w-full flex flex-col justify-start items-start gap-5 fixed left-1/2 top-24 border-none bg-[#000] lg:Navbar_Links_Ul lg:flex lg:flex-row lg:justify-center 
+    lg:w-auto lg:h-auto lg:py-2 lg:px-12 lg:bg-[#ffffff33]  lg:backdrop-blur-xl lg:rounded-3xl border lg:border-[#ffffff33] lg:start-1/2 -translate-x-2/4 lg:top-8`;
 
   return (
     <Fragment>
@@ -36,17 +35,27 @@ function Navbar() {
           <ul
             style={{ transition: "all 0.3s ease-in-out" }}
             className={`${navbarDefaultClass} ${
-              navbarOpen
-                ? "w-full h-full left-1/2 top-24"
-                : "w-full h-0 left-1/2 top-24"
-            }`}
+              navbarOpen ? "h-full" : "h-0"
+            } `}
           >
             {linksArray.map((element, index) => (
               <li
                 key={index}
                 style={{ transition: "all 0.3s ease-in-out" }}
-                className={`Navbar_Li text-2xl tracking-tight py-8 px-10 mt-5 text-[#ffffffcc] hover:text-[#ffffff] lg:mt-0 lg:px-2 lg:py-2 lg:text-sm ${
-                  navbarOpen ? "opacity-1" : "opacity-0 -mb-5"
+                className={`Navbar_Li hidden invisible text-sm tracking-tight mt-0 px-2 py-2 lg:block lg:visible text-[#ffffffcc] hover:text-[#ffffff] `}
+              >
+                <a href="#" className="Navbar_Link px-2 py-2">
+                  {element}
+                </a>
+              </li>
+            ))}
+
+            {linksArray.map((element, index) => (
+              <li
+                key={index}
+                style={{ transition: "all 0.3s ease-in-out" }}
+                className={`Offcanvas_Li block visible text-2xl opacity-1 tracking-tight py-8 px-10 mt-5 lg:hidden lg:invisible text-[#ffffffcc] hover:text-[#ffffff] ${
+                  navbarOpen ? "opacity-1 mt-0" : "opacity-0 mt-60"
                 }`}
               >
                 <a href="#" className="Navbar_Link px-2 py-2">
@@ -69,48 +78,29 @@ function Navbar() {
           </a>
 
           <div
-            className="flex flex-col justify-center items-end gap-1 relative px-5 py-5 lg:hidden"
-            onClick={NavbrOpenFunctionility}
+            className="w-[60px] h-[45px] relative mt-[30px] mx-auto transform rotate-0 transition-all ease-in-out duration-300 lg:hidden"
+            onClick={(e) => {
+              e.target.classList.toggle("open");
+              NavbrOpenFunctionility();
+            }}
           >
-            {navbarOpen ? (
-              <>
-                <span className="w-6 md:w-12 h-1 border-t-2 -rotate-45"></span>
-                <span className="w-6 md:w-12 h-1 border-t-2 rotate-45"></span>
-              </>
-            ) : (
-              <>
-                <span className="w-5 md:w-7 h-1 border-t-2"></span>
-                <span className="w-7 md:w-9 h-1 border-t-2"></span>
-              </>
-            )}
+            <span
+              className={`${`origin-left block absolute h-[3px] bg-[#fff] rounded-[9px] opacity-100 transition-all ease-in-out duration-300`}
+${
+  navbarOpen
+    ? "transform rotate-45 top-[-3px] left-[8px] w-[60px]"
+    : "transform rotate-0  top-0 right-0 w-[35px]"
+}`}
+            ></span>
 
-            {/* <button x-data="{ open: false }"
-          x-on:click="open = !open"
-          class="flex items-center space-x-2 focus:outline-none">
-    <span x-text="open ? 'Close' : 'Open'"
-          class="font-medium text-lg">Open</span>
-    
-    <div class="w-6 flex items-center justify-center relative">
-      <span x-bind:class="open ? 'translate-y-0 rotate-45' : '-translate-y-2'"
-            class="transform transition w-full h-px bg-current absolute"></span>
-      
-      <span x-bind:class="open ? 'opacity-0 translate-x-3' : 'opacity-100'"
-            class="transform transition w-full h-px bg-current absolute"></span>
-      
-      <span x-bind:class="open ? 'translate-y-0 -rotate-45' : 'translate-y-2'"
-            class="transform transition w-full h-px bg-current absolute"></span>
-    </div>
-  </button> */}
-
-            {/* <input
-              type="checkbox"
-              className="absolute w-full h-full left-0 top-0 opacity-1 z-50 cursor-pointer"
-              onClick={(e) => {
-                console.log(this);
-
-                e.target.checked ? setNavbarOpen(true) : setNavbarOpen(false);
-              }}
-            /> */}
+            <span
+              className={`${`origin-left block absolute h-[3px] bg-[#fff] w-full rounded-[9px] opacity-100 left-0 transition-all ease-in-out duration-300`}
+${
+  navbarOpen
+    ? "transform -rotate-45 top-[39px] left-[8px]"
+    : "transform rotate-0 top-[15px] left-0"
+}`}
+            ></span>
           </div>
         </div>
       </nav>
@@ -119,6 +109,8 @@ function Navbar() {
 }
 
 export default Navbar;
+
+// transform rotate-45 top-[-3px] left-[8px]
 
 // import React, { useState } from 'react';
 
