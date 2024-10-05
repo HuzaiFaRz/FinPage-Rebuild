ca147c17e6b1bfae11a2cbabb2a9097bc750b674
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-10-05 10:32:03 +0500

    S

--- src/Components/SpecialHeading.jsx
+++ src/Components/SpecialHeading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function SpecialHeading({ leftText, specialText, centerText, lineBreak }) {
   return (
-    <h1 className="font-sora pb-5 text-center w-full text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-light tracking-tighter leading-tight">
+    <h1 className="font-sora pb-5 text-center w-full text-4xl sm:text-5xl md:text-6xl lg:text-7xl text-white font-light tracking-tighter">
       {leftText}{" "}
       <span className="bg-gradient-to-b from-[#00c7e8] via-[#0bb4ea] to-[#7004ff] bg-clip-text  text-transparent">
         {specialText}
