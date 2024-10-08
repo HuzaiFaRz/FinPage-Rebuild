f64a757c23d5aad8aa63c47cb2f31f6e84c77b20
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-10-08 20:29:25 +0500

    S

--- src/Components/Changelog.jsx
+++ src/Components/Changelog.jsx
@@ -7,7 +7,7 @@ function ChangeLog() {
   return (
     <Fragment>
       <div className="bg-[#000] font-dmsans px-10 sm:px-16 md:px-28 py-28 flex flex-col justify-center items-start gap-10">
-        <Heading text="Changelog" />
+        <Heading text="ChangeLog" />
         <Paragraph text="If there is any update or change to the template, you'll find it here." />
         <Heading text="Version 1.0" />
         <Paragraph text="Initial Release" />
