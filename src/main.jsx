eb418fdb4713c5c1113943c52cf9723ac231b9f1
HuzaiFa Raza <huziafarazaa999@gmail.com>
2024-10-11 21:26:09 +0500

    S

--- src/main.jsx
+++ src/main.jsx
@@ -1,53 +1,41 @@
-// import { StrictMode } from "react";
-// import { createRoot } from "react-dom/client";
-// import "../index.css";
-// import { createBrowserRouter, RouterProvider } from "react-router-dom";
-// import App from "./App";
-// import Navbar from "./Components/Navbar";
-// import StyleGuid from "./Components/StyleGuid";
-// import License from "./Components/License";
-// import ChangeLog from "./Components/ChangeLog";
-// import Error from "./Components/Error";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "../index.css";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App";
+import Navbar from "./Components/Navbar";
+import StyleGuid from "./Components/StyleGuid";
+import License from "./Components/License";
+import ChangeLog from "./Components/ChangeLog";
+import Error from "./Components/Error";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <Navbar />,
-//     errorElement: <Error />,
-//     children: [
-//       {
-//         path: "/",
-//         element: <App />,
-//       },
-//       {
-//         path: "StyleGuide",
-//         element: <StyleGuid />,
-//       },
-//       {
-//         path: "License",
-//         element: <License />,
-//       },
-//       {
-//         path: "ChangeLog",
-//         element: <ChangeLog />,
-//       },
-//     ],
-//   },
-// ]);
-// createRoot(document.getElementById("root")).render(
-//   <StrictMode>
-//     <RouterProvider router={router} />
-//   </StrictMode>
-// );
-
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import '../index.css'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navbar />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+      },
+      {
+        path: "StyleGuide",
+        element: <StyleGuid />,
+      },
+      {
+        path: "License",
+        element: <License />,
+      },
+      {
+        path: "ChangeLog",
+        element: <ChangeLog />,
+      },
+    ],
+  },
+]);
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+);
